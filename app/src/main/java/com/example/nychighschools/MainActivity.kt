119b3aa809742f647736d9e82c5c3d6aff3c0be5
Jonathan <jonathan.maldonado@exprealty.net>
2023-06-10 13:46:54 -0600

    add clickable basic functionality as well as details card

--- app/src/main/java/com/example/nychighschools/MainActivity.kt
+++ app/src/main/java/com/example/nychighschools/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nychighschools
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -18,12 +19,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.School
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -38,10 +42,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -51,6 +57,7 @@ import com.example.nychighschools.ui.theme.NYCHighSchoolsTheme
 
 
 //data class School(val name: String, val building: String? = null, val location: String)
+data class NavItem(val title: String, val icon: ImageVector)
 
 
 class MainActivity : ComponentActivity() {
@@ -75,7 +82,9 @@ class MainActivity : ComponentActivity() {
 fun MyApp() {
 
 
-    val schools = remember { mutableStateListOf<School>() }
+    val schools = mutableListOf<School>()
+    //there is definetly  a better way of saving wich school is selected but this is the first aproach that comes to mind I will update it if possible
+    var selectedSchool by rememberSaveable { mutableStateOf(-1) }
 
     //a hacky way to get a big list
     for (i in 1..100) {
@@ -85,35 +94,42 @@ fun MyApp() {
 
     }
 
-    LazyColumn {
-        items(schools) { school ->
-            SchoolCard(school, modifier = Modifier.clickable {
-                schools.add(School(schoolName = "Women's Academy of Excellence", location = "456 White Plains Road, Bronx NY 10473 (40.815043, -73.85607)"))
-            })
+    if (selectedSchool >= 0) {
+        SchoolDetails(school = schools[selectedSchool], onBackClicked = { selectedSchool = -1 }, modifier = Modifier)
+    } else {
+        LazyColumn {
+            itemsIndexed(schools) { index, school ->
+                SchoolCard(school, modifier = Modifier.clickable {
+                    Log.d("Jon", "selectedSchool: $selectedSchool")
+                    selectedSchool = index;
+                    //  schools.add(School(schoolName = "Women's Academy of Excellence", location = "456 White Plains Road, Bronx NY 10473 (40.815043, -73.85607)"))
+                })
+            }
         }
     }
+
 
 }
 
 @Composable
-fun SchoolCard(school: School, modifier: Modifier = Modifier) {
+fun SchoolCard(school: School, modifier: Modifier) {
 
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.background
         ),
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp, ).shadow(elevation = 8.dp)
+        modifier = modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .shadow(elevation = 8.dp)
     ) {
         CardContent(school)
     }
 
 
-
-
 }
 
 @Composable
-private fun CardContent( school: School, modifier: Modifier = Modifier) {
+private fun CardContent(school: School, modifier: Modifier = Modifier) {
 
 
     var isFavorite by rememberSaveable { mutableStateOf(false) }
@@ -121,76 +137,74 @@ private fun CardContent( school: School, modifier: Modifier = Modifier) {
     Column {
 
 
-            Row(modifier = Modifier.padding(all = 8.dp)) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
 
-                Icon(
-                    imageVector = Icons.Filled.School,
-                    contentDescription = stringResource(R.string.school_favourite),
-                    tint = if (isFavorite) Color.Red else Color.Gray,
-                    modifier = Modifier
-                        // Set image size to 40 dp
-                        .size(80.dp).border(1.5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp))
+            Icon(
+                imageVector = Icons.Filled.School,
+                contentDescription = stringResource(R.string.school_favourite),
+                tint = Color.Gray,
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(80.dp)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp))
+            )
+
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+
+
+
+            Column {
+
+                Text(
+                    text = school.schoolName,
+                    modifier = modifier,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.Black
                 )
 
+                Text(
+                    text = school.location,
+                    modifier = modifier,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyMedium,
 
-                Spacer(modifier = Modifier.width(16.dp))
 
-
-
-
-                Column {
-
-                    Text(
-                        text = school.schoolName,
-                        modifier = modifier,
-                        style = MaterialTheme.typography.titleMedium,
-                        color = Color.Black
                     )
 
+                if (school.buildingCode.isNotEmpty()) {
                     Text(
-                        text = school.location,
+                        text = stringResource(R.string.building_code, school.buildingCode),
                         modifier = modifier,
                         color = MaterialTheme.colorScheme.secondary,
                         style = MaterialTheme.typography.bodyMedium,
 
-
                         )
-
-                    if (school.buildingCode.isNotEmpty()) {
-                        Text(
-                            text = stringResource(R.string.building_code, school.buildingCode),
-                            modifier = modifier,
-                            color = MaterialTheme.colorScheme.secondary,
-                            style = MaterialTheme.typography.bodyMedium,
-
-                            )
-                    }
-
-
-
-
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.End
-                    ) {
-                        IconButton(
-                            onClick = { isFavorite = !isFavorite },
-                            modifier = Modifier
-                        ) {
-                            Icon(
-                                imageVector = if (isFavorite) Icons.Filled.Star else Icons.Filled.StarBorder,
-                                contentDescription = stringResource(R.string.school_favourite),
-                                tint = if (isFavorite) Color.Red else Color.Gray
-                            )
-                        }
-                    }
-
-
                 }
+
+
+
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(
+                        onClick = { isFavorite = !isFavorite },
+                        modifier = Modifier
+                    ) {
+                        Icon(
+                            imageVector = if (isFavorite) Icons.Filled.Star else Icons.Filled.StarBorder,
+                            contentDescription = stringResource(R.string.school_favourite),
+                            tint = if (isFavorite) Color.Red else Color.Gray
+                        )
+                    }
+                }
+
+
             }
-
-
-
+        }
 
 
     }
@@ -198,17 +212,107 @@ private fun CardContent( school: School, modifier: Modifier = Modifier) {
 }
 
 
-fun getIsFavoriteColor(isFavorite: Boolean): Color {
-    return if (isFavorite) Color.Red else Color.Black
+@Composable
+private fun CardContentDetails(school: School, modifier: Modifier = Modifier) {
+
+
+
+
+    Column {
+
+
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+
+            Icon(
+                imageVector = Icons.Filled.Search,
+                contentDescription = stringResource(R.string.school_favourite),
+                tint = Color.Gray,
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(80.dp)
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp))
+            )
+
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+
+
+
+            Column {
+
+                Text(
+                    text = school.schoolName,
+                    modifier = modifier,
+                    style = MaterialTheme.typography.titleMedium,
+                    color = Color.Black
+                )
+
+                Text(
+                    text = school.location,
+                    modifier = modifier,
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyMedium,
+
+
+                    )
+
+                if (school.buildingCode.isNotEmpty()) {
+                    Text(
+                        text = stringResource(R.string.building_code, school.buildingCode),
+                        modifier = modifier,
+                        color = MaterialTheme.colorScheme.secondary,
+                        style = MaterialTheme.typography.bodyMedium,
+
+                        )
+                }
+
+
+
+            }
+        }
+
+
+    }
+
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
-)
 
 @Composable
-fun GreetingPreview() {
+fun SchoolDetails(
+    school: School,
+    modifier: Modifier, onBackClicked: () -> Unit,
+) {
+
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+            .shadow(elevation = 8.dp).clickable{onBackClicked()}
+    ) {
+        CardContentDetails(school, modifier)
+    }
+}
+
+
+@Preview()
+@Composable
+fun MyAppPreview() {
 
     MyApp()
+}
+
+@Preview()
+@Composable
+fun SchoolDetailsPreview() {
+
+    val schools = mutableListOf<School>()
+
+    schools.add(School(schoolName = "Clinton School Writers & Artists, M.S. 260", location = "10 East 15th Street, Manhattan NY 10003 (40.736526, -73.992727)", buildingCode = "M868"))
+
+
+    SchoolDetails(school = schools[0], onBackClicked = {  }, modifier = Modifier)
 }
