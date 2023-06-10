41ed684d70ffe9ba22e032abe76134b483c63aa4
Jonathan <jonathan.maldonado@exprealty.net>
2023-06-10 13:04:43 -0600

    update UI

--- app/src/main/java/com/example/nychighschools/MainActivity.kt
+++ app/src/main/java/com/example/nychighschools/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,10 +20,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.School
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.filled.StarBorder
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,11 +38,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -62,8 +62,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
-                    SetUi()
-
+                    MyApp()
 
                 }
             }
@@ -73,7 +72,7 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun SetUi() {
+fun MyApp() {
 
 
     val schools = remember { mutableStateListOf<School>() }
@@ -99,21 +98,38 @@ fun SetUi() {
 @Composable
 fun SchoolCard(school: School, modifier: Modifier = Modifier) {
 
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp, ).shadow(elevation = 8.dp)
+    ) {
+        CardContent(school)
+    }
+
+
+
+
+}
+
+@Composable
+private fun CardContent( school: School, modifier: Modifier = Modifier) {
+
 
     var isFavorite by rememberSaveable { mutableStateOf(false) }
 
     Column {
 
 
-        Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 8.dp, modifier = Modifier.padding(8.dp)) {
             Row(modifier = Modifier.padding(all = 8.dp)) {
-                Image(
-                    painter = painterResource(R.drawable.high_school_icon), contentDescription = "School picture", modifier = Modifier
+
+                Icon(
+                    imageVector = Icons.Filled.School,
+                    contentDescription = stringResource(R.string.school_favourite),
+                    tint = if (isFavorite) Color.Red else Color.Gray,
+                    modifier = Modifier
                         // Set image size to 40 dp
-                        .size(80.dp)
-                        // Clip image to be shaped as a circle
-                        .clip(RoundedCornerShape(2.5.dp))
-                        .border(1.5.dp, MaterialTheme.colorScheme.primary)
+                        .size(80.dp).border(1.5.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(4.dp))
                 )
 
 
@@ -174,11 +190,10 @@ fun SchoolCard(school: School, modifier: Modifier = Modifier) {
             }
 
 
-        }
+
 
 
     }
-
 
 }
 
@@ -195,5 +210,5 @@ fun getIsFavoriteColor(isFavorite: Boolean): Color {
 @Composable
 fun GreetingPreview() {
 
-    SetUi()
+    MyApp()
 }
